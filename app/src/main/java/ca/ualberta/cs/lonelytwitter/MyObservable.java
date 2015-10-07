29a4d066bc49dc79f9864b7fb81553dfca4280e2
Carson McLean <camclean@ualberta.ca>
2015-10-07 17:16:10 -0600

    Added Observable MVC style Interfaces. Also curious to test if my GH is broken?

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/MyObservable.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by carsonmclean on 7/10/15.
+ */
+public interface MyObservable {
+    private ArrayList<MyObserver> observers;
+
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/MyObserver.java
@@ -0,0 +1,7 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by carsonmclean on 7/10/15.
+ */
+public interface MyObserver {
+}
